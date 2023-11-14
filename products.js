7cf9a30cb0d473c0439dc7c0c24eb0ea8170d883
efraín <efrae@hotmail.com>
2023-11-13 23:19:17 -0300

    Updates

---
+++ products.js
@@ -0,0 +1,7 @@
+[
+ { "id":1, "nombre": "Buzo", "precio": 18000, "img": "/img/buzo adidas.jpg", "categoria":"buzos y camperas","descripcion": "Descripcion del producto", "stock": 10},
+ { "id":2, "nombre": "Pantalón", "precio": 9500, "img": "/img/pantalon wrangler.jpg", "categoria":"pantalones","descripcion": "Descripcion del producto", "stock": 10},
+ { "id":3, "nombre": "Remera", "precio": 7000 , "img": "/img/remera 1.png", "categoria":"remeras","descripcion": "Descripcion del producto", "stock": 10},
+ { "id":4, "nombre": "Remera", "precio": 10000, "img": "/img/remera 2.jpg", "categoria":"remeras", "descripcion":"Descripcion del producto", "stock": 10},
+ { "id":5, "nombre": "Zapatillas", "precio": 30000, "img": "/img/zapatillas.jpeg", "categoria":"calzado","descripcion": "Descripcion del producto", "stock": 10}
+]
